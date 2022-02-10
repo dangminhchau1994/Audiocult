fd1c3e0eb96994be75c2753ae30c6cb5c6aebdbc
dangminhchau <ldcc.minhchau@lotte.vn>
2022-02-10 14:33:29 +0700

    common button

---
+++ lib/app/constants/app_assets.dart
@@ -0,0 +1,3 @@
+class AppAssets {
+  static const pdfIcon = 'assets/icons/pdf.svg';
+}
---
+++ lib/widgets/common_button.dart
@@ -0,0 +1,44 @@
+import 'package:audio_cult/app/constants/app_assets.dart';
+import 'package:flutter/material.dart';
+
+class CommonButton extends StatelessWidget {
+  const CommonButton({
+    Key? key,
+    this.text,
+    this.color,
+    this.onTap,
+    this.iconPath = '',
+    this.width = 343.0,
+  }) : super(key: key);
+
+  final String? text;
+  final Color? color;
+  final Function()? onTap;
+  final double? width;
+  final String? iconPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+        ),
+        onPressed: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            text ?? '',
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
