5dc646575793653ecde6065cc582da1d39942eda
MienHoang <E108484>
2022-03-02 12:48:15 +0700

    hot fix

--- lib/app/data_source/models/responses/song/song_response.g.dart
+++ lib/app/data_source/models/responses/song/song_response.g.dart
@@ -58,7 +58,7 @@ _$_Song _$$_SongFromJson(Map<String, dynamic> json) => _$_Song(
       totalDownload: json['totalDownload'] as String?,
       tags: json['tags'] as String?,
       imagePath: json['image_path'] as String?,
-      artistUser: json['artist_user'] == null
+      artistUser: json['artist_user'] == null || !(json['artist_user'] is Map<String, dynamic>)
           ? null
           : ArtistUser.fromJson(json['artist_user'] as Map<String, dynamic>),
       peaksJsonUrl: json['peaksJsonUrl'] as String?,
