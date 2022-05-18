e718969f84f41221a6f473bc45b56aba81ad7f47
dangminhchau <ldcc.minhchau@lotte.vn>
2022-05-18 16:41:41 +0700

    notifications

--- lib/app/features/notifications/widgets/notification_item.dart
+++ lib/app/features/notifications/widgets/notification_item.dart
@@ -18,76 +18,78 @@ class NotificationItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ExpandablePanel(
-      theme: const ExpandableThemeData(
-        hasIcon: false,
-        tapHeaderToExpand: false,
-        tapBodyToCollapse: false,
-        tapBodyToExpand: false,
-        useInkWell: false,
-      ),
-      controller: ExpandableController(initialExpanded: true),
-      header: Text(
-        data?.date ?? '',
-        style: context.bodyTextPrimaryStyle()!.copyWith(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-      ),
-      expanded: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: data?.notifications?.length ?? 0,
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                color: data?.notifications?[index].isRead == '0' ? AppColors.primaryButtonColor : Colors.transparent,
+        theme: const ExpandableThemeData(
+          hasIcon: false,
+          tapHeaderToExpand: false,
+          tapBodyToCollapse: false,
+          tapBodyToExpand: false,
+          useInkWell: false,
+        ),
+        controller: ExpandableController(initialExpanded: true),
+        header: Text(
+          data?.date ?? '',
+          style: context.bodyTextPrimaryStyle()!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 14),
-                child: ListTile(
-                  leading: CachedNetworkImage(
-                    width: 50,
-                    height: 50,
-                    imageUrl: data?.notifications?[index].userImage ?? '',
-                    imageBuilder: (context, imageProvider) => Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: imageProvider,
-                          fit: BoxFit.cover,
-                        ),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    placeholder: (context, url) => Center(
-                      child: CircularProgressIndicator(
-                        color: AppColors.primaryButtonColor,
-                      ),
-                    ),
-                    errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+        expanded: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: data?.notifications?.length ?? 0,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: data?.notifications?[index].isRead == '0' ? AppColors.primaryButtonColor : Colors.transparent,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 14,
+                    bottom: 14,
                   ),
-                  title: Html(
-                    data: data?.notifications?[index].message,
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      data?.date ?? '',
-                      style: context.bodyTextPrimaryStyle()!.copyWith(
-                            color: Colors.white,
-                            fontSize: 16,
+                  child: ListTile(
+                    leading: CachedNetworkImage(
+                      width: 50,
+                      height: 50,
+                      imageUrl: data?.notifications?[index].userImage ?? '',
+                      imageBuilder: (context, imageProvider) => Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: imageProvider,
+                            fit: BoxFit.cover,
                           ),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      placeholder: (context, url) => Center(
+                        child: CircularProgressIndicator(
+                          color: AppColors.primaryButtonColor,
+                        ),
+                      ),
+                      errorWidget: (context, url, error) => const Icon(Icons.error),
+                    ),
+                    title: Html(
+                      data: data?.notifications?[index].message,
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        data?.date ?? '',
+                        style: context.bodyTextPrimaryStyle()!.copyWith(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-      ),
-      collapsed: Container(),
-    );
+        collapsed: Container());
   }
 }
