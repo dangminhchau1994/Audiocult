336736099d87039da512bc06f92e063bdde70f5f
dangminhchau <ldcc.minhchau@lotte.vn>
2022-05-23 16:55:21 +0700

    event calendar

--- lib/app/features/events/calendar/calendar_screen.dart
+++ lib/app/features/events/calendar/calendar_screen.dart
@@ -1,27 +1,25 @@
 import 'package:audio_cult/app/features/events/calendar/calendar_bloc.dart';
-import 'package:audio_cult/app/features/events/calendar/widgets/calendar_event_item.dart';
+import 'package:audio_cult/app/features/events/calendar/widgets/calendar_event_list.dart';
+import 'package:audio_cult/app/features/events/calendar/widgets/calendar_table.dart';
 import 'package:audio_cult/app/utils/constants/app_colors.dart';
 import 'package:audio_cult/app/utils/constants/app_dimens.dart';
-import 'package:audio_cult/app/view/no_data_widget.dart';
 import 'package:audio_cult/l10n/l10n.dart';
 import 'package:audio_cult/w_components/appbar/common_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:intl/intl.dart';
+import 'package:multi_value_listenable_builder/multi_value_listenable_builder.dart';
 import 'package:table_calendar/table_calendar.dart';
-
 import '../../../../di/bloc_locator.dart';
 import '../../../../w_components/buttons/w_button_inkwell.dart';
-import '../../../../w_components/loading/loading_builder.dart';
-import '../../../../w_components/loading/loading_widget.dart';
 import '../../../constants/global_constants.dart';
 import '../../../data_source/models/requests/event_request.dart';
 import '../../../data_source/models/responses/events/event_response.dart';
 import '../../../utils/constants/app_assets.dart';
 import '../../../utils/debouncer.dart';
+import '../../../utils/extensions/app_extensions.dart';
 import '../../../utils/route/app_route.dart';
-import '../../music/library/widgets/empty_playlist.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
@@ -35,15 +33,20 @@ class _CalendarScreenState extends State<CalendarScreen> {
   late FocusNode focusNode;
   late Debouncer debouncer;
   late CalendarBloc _calendarBloc;
+  final _rangeStart = DateTime.now();
+  final _rangeEnd = DateTime.now();
   final ValueNotifier<String> _text = ValueNotifier<String>('');
   final PagingController<int, EventResponse> _pagingController = PagingController(firstPageKey: 1);
-  CalendarFormat _calendarFormat = CalendarFormat.week;
-  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOn;
-  DateTime currentDate = DateTime.now();
-  DateTime? _selectedDay;
-  DateTime? _rangeStart = DateTime.now();
-  DateTime? _rangeEnd = DateTime.now();
-  bool _todaySelected = true;
+  final List<ValueNotifier<dynamic>> _listCalendar = [
+    ValueNotifier(DateTime.now()),
+    ValueNotifier(DateTime(0)),
+    ValueNotifier(DateTime.now()),
+    ValueNotifier(DateTime.now()),
+    ValueNotifier(true),
+    ValueNotifier(CalendarFormat.week),
+    ValueNotifier(RangeSelectionMode.toggledOn),
+    ValueNotifier('')
+  ];
 
   @override
   void initState() {
@@ -57,7 +60,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
       }
     });
     _calendarBloc = getIt.get<CalendarBloc>();
-    callData('', 1, currentDate, currentDate);
+    callData('', 1, DateTime.now(), DateTime.now());
   }
 
   void callData(String query, int page, DateTime startDate, DateTime endDate) {
@@ -65,7 +68,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
     _calendarBloc.requestData(
       params: EventRequest(
         query: query,
-        sort: 'most-liked',
+        sort: 'latest',
         startTime: DateFormat('yyyy/MM/dd 00:00').format(startDate),
         endTime: DateFormat('yyyy/MM/dd 23:59').format(endDate),
         page: 1,
@@ -79,9 +82,9 @@ class _CalendarScreenState extends State<CalendarScreen> {
       final newItems = await _calendarBloc.loadData(
         EventRequest(
           query: _text.value,
-          sort: 'most-liked',
-          startTime: DateFormat('yyyy/MM/dd 00:00').format(_rangeStart!),
-          endTime: DateFormat('yyyy/MM/dd 23:59').format(_rangeEnd!),
+          sort: 'latest',
+          startTime: DateFormat('yyyy/MM/dd 00:00').format(_rangeStart),
+          endTime: DateFormat('yyyy/MM/dd 23:59').format(_rangeEnd),
           page: pageKey,
           limit: GlobalConstants.loadMoreItem,
         ),
@@ -105,190 +108,70 @@ class _CalendarScreenState extends State<CalendarScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.mainColor,
-      appBar: CommonAppBar(
-        title: context.l10n.t_calendar,
-      ),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: AppColors.secondaryButtonColor,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(kHorizontalSpacing),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      _buildSearchInput(),
-                      const SizedBox(width: 10),
-                      _buildFilter(),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  _buildTableCalendar(),
-                ],
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.mainColor,
+        appBar: CommonAppBar(
+          title: context.l10n.t_calendar,
+        ),
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.secondaryButtonColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(kHorizontalSpacing),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        _buildSearchInput(),
+                        const SizedBox(width: 10),
+                        _buildFilter(),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    CalendarTable(
+                      listCalendar: _listCalendar,
+                      callData: callData,
+                      query: _text.value,
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-              color: AppColors.primaryButtonColor,
-              backgroundColor: AppColors.secondaryButtonColor,
-              onRefresh: () async {
-                _pagingController.refresh();
-                callData(_text.value, 1, _rangeStart!, _rangeEnd!);
-              },
-              child: LoadingBuilder<CalendarBloc, List<EventResponse>>(
-                noDataBuilder: (state) {
-                  return const NoDataWidget();
-                },
-                builder: (data, _) {
-                  //only first page
-                  final isLastPage = data.length == GlobalConstants.loadMoreItem - 1;
-                  if (isLastPage) {
-                    _pagingController.appendLastPage(data);
-                  } else {
-                    _pagingController.appendPage(data, _pagingController.firstPageKey + 1);
-                  }
-                  return PagedListView<int, EventResponse>.separated(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 16,
-                    ),
-                    pagingController: _pagingController,
-                    separatorBuilder: (context, index) => const Divider(height: 24),
-                    builderDelegate: PagedChildBuilderDelegate<EventResponse>(
-                      firstPageProgressIndicatorBuilder: (context) => Container(),
-                      newPageProgressIndicatorBuilder: (context) => const LoadingWidget(),
-                      animateTransitions: true,
-                      itemBuilder: (context, item, index) {
-                        return CalendarEventItem(
-                          data: item,
-                        );
-                      },
-                    ),
-                  );
-                },
-                reloadAction: (_) {
-                  _pagingController.refresh();
-                  callData('', 1, currentDate, currentDate);
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildFilter() {
-    return WButtonInkwell(
-      onPressed: () {
-        Navigator.pushNamed(context, AppRoute.routeFilterEvent);
-      },
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: AppColors.inputFillColor,
-        ),
-        child: SvgPicture.asset(
-          AppAssets.filterIcon,
-          width: 30,
-          height: 30,
+            CalendarEventList(
+              callData: callData,
+              pagingController: _pagingController,
+              rangeStart: _rangeStart,
+              rangeEnd: _rangeEnd,
+            )
+          ],
         ),
       ),
-    );
-  }
-
-  Widget _buildTableCalendar() {
-    return TableCalendar(
-      firstDay: DateTime.utc(2010, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: currentDate,
-      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-      rangeStartDay: _rangeStart,
-      rangeEndDay: _rangeEnd,
-      calendarFormat: _calendarFormat,
-      headerVisible: true,
-      daysOfWeekStyle: const DaysOfWeekStyle(
-        weekdayStyle: TextStyle(color: Colors.white),
-        weekendStyle: TextStyle(color: Colors.white),
-      ),
-      calendarStyle: CalendarStyle(
-        disabledTextStyle: const TextStyle(color: Colors.white),
-        selectedTextStyle: const TextStyle(color: Colors.white),
-        weekendTextStyle: const TextStyle(color: Colors.white),
-        rangeStartDecoration: BoxDecoration(
-          color: AppColors.activeLabelItem,
-          shape: BoxShape.circle,
-        ),
-        rangeEndDecoration: BoxDecoration(
-          color: AppColors.activeLabelItem,
-          shape: BoxShape.circle,
-        ),
-        todayDecoration: BoxDecoration(
-          color: _todaySelected ? AppColors.activeLabelItem : Colors.transparent,
-          shape: BoxShape.circle,
-        ),
-        selectedDecoration: BoxDecoration(
-          color: AppColors.activeLabelItem,
-          shape: BoxShape.circle,
-        ),
-      ),
-      rangeSelectionMode: _rangeSelectionMode,
-      onDaySelected: (selectedDay, focusedDay) {
-        if (!isSameDay(_selectedDay, selectedDay)) {
-          setState(() {
-            _selectedDay = selectedDay;
-            currentDate = focusedDay;
-            _rangeStart = null; // Important to clean those
-            _rangeEnd = null;
-            _todaySelected = false;
-            _rangeSelectionMode = RangeSelectionMode.toggledOff;
-          });
-        }
-      },
-      onRangeSelected: (start, end, focusedDay) {
-        setState(() {
-          _selectedDay = null;
-          currentDate = focusedDay;
-          _rangeStart = start;
-          _rangeEnd = end;
-          _todaySelected = false;
-          _rangeSelectionMode = RangeSelectionMode.toggledOn;
-        });
-        callData('', 1, _rangeStart!, _rangeEnd!);
-      },
-      onFormatChanged: (format) {
-        if (_calendarFormat != format) {
-          setState(() {
-            _calendarFormat = format;
-          });
-        }
-      },
-      onPageChanged: (focusedDay) {
-        currentDate = focusedDay;
-      },
     );
   }
 
   Widget _buildSearchInput() {
     return Flexible(
-      child: ValueListenableBuilder<String>(
-        valueListenable: _text,
-        builder: (context, query, child) {
+      child: MultiValueListenableBuilder(
+        valueListenables: _listCalendar,
+        builder: (context, values, child) {
+          final query = asType<String>(values.elementAt(7));
+          final start = asType<DateTime>(values.elementAt(2));
+          final end = asType<DateTime>(values.elementAt(3));
+
           return TextField(
             controller: editingController,
             cursorColor: Colors.white,
             onChanged: (value) {
-              _text.value = value;
-              callData(_text.value, 1, _rangeStart!, _rangeEnd!);
+              _listCalendar[7].value = value;
+              callData(value, 1, start!, end!);
             },
             decoration: InputDecoration(
               filled: true,
@@ -311,8 +194,8 @@ class _CalendarScreenState extends State<CalendarScreen> {
               hintText: context.l10n.t_search,
               prefixIcon: GestureDetector(
                 onTap: () {
-                  if (query.isNotEmpty) {
-                    callData(query, 1, _rangeStart!, _rangeEnd!);
+                  if (query!.isNotEmpty) {
+                    callData(query, 1, start!, end!);
                   }
                 },
                 child: const Icon(
@@ -325,13 +208,13 @@ class _CalendarScreenState extends State<CalendarScreen> {
                 top: 20,
                 left: 10,
               ),
-              suffixIcon: query.isNotEmpty
+              suffixIcon: query!.isNotEmpty
                   ? GestureDetector(
                       onTap: () {
                         focusNode.unfocus();
                         editingController.text = '';
-                        _text.value = '';
-                        callData('', 1, _rangeStart!, _rangeEnd!);
+                        _listCalendar[7].value = '';
+                        callData('', 1, start!, end!);
                       },
                       child: const Icon(
                         Icons.close,
@@ -343,6 +226,26 @@ class _CalendarScreenState extends State<CalendarScreen> {
             ),
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildFilter() {
+    return WButtonInkwell(
+      onPressed: () {
+        Navigator.pushNamed(context, AppRoute.routeFilterEvent);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: AppColors.inputFillColor,
+        ),
+        child: SvgPicture.asset(
+          AppAssets.filterIcon,
+          width: 30,
+          height: 30,
+        ),
       ),
     );
   }
---
+++ lib/app/features/events/calendar/widgets/calendar_table.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:multi_value_listenable_builder/multi_value_listenable_builder.dart';
+import 'package:table_calendar/table_calendar.dart';
+import '../../../../utils/constants/app_colors.dart';
+import '../../../../utils/extensions/app_extensions.dart';
+
+class CalendarTable extends StatefulWidget {
+  const CalendarTable({
+    Key? key,
+    this.listCalendar,
+    this.query,
+    this.callData,
+  }) : super(key: key);
+
+  final List<ValueNotifier<dynamic>>? listCalendar;
+  final String? query;
+  final Function(String query, int page, DateTime startDate, DateTime endDate)? callData;
+
+  @override
+  State<CalendarTable> createState() => _CalendarTableState();
+}
+
+class _CalendarTableState extends State<CalendarTable> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiValueListenableBuilder(
+      valueListenables: widget.listCalendar!,
+      builder: (context, values, child) {
+        final currentDate = asType<DateTime>(values.elementAt(0));
+        final selectedDate = asType<DateTime>(values.elementAt(1));
+        final rangeStartDate = asType<DateTime>(values.elementAt(2));
+        final rangeEndDate = asType<DateTime>(values.elementAt(3));
+        final todaySelected = asType<bool>(values.elementAt(4));
+        final formatCalendar = asType<CalendarFormat>(values.elementAt(5));
+        final rangeSelectionMode = asType<RangeSelectionMode>(values.elementAt(6));
+
+        return TableCalendar(
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: currentDate!,
+          selectedDayPredicate: (day) => isSameDay(selectedDate, day),
+          rangeStartDay: rangeStartDate,
+          rangeEndDay: rangeEndDate,
+          calendarFormat: formatCalendar!,
+          headerVisible: true,
+          daysOfWeekStyle: const DaysOfWeekStyle(
+            weekdayStyle: TextStyle(color: Colors.white),
+            weekendStyle: TextStyle(color: Colors.white),
+          ),
+          calendarStyle: CalendarStyle(
+            disabledTextStyle: const TextStyle(color: Colors.white),
+            selectedTextStyle: const TextStyle(color: Colors.white),
+            weekendTextStyle: const TextStyle(color: Colors.white),
+            rangeStartDecoration: BoxDecoration(
+              color: AppColors.activeLabelItem,
+              shape: BoxShape.circle,
+            ),
+            rangeEndDecoration: BoxDecoration(
+              color: AppColors.activeLabelItem,
+              shape: BoxShape.circle,
+            ),
+            todayDecoration: BoxDecoration(
+              color: todaySelected! ? AppColors.activeLabelItem : Colors.transparent,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: AppColors.activeLabelItem,
+              shape: BoxShape.circle,
+            ),
+          ),
+          rangeSelectionMode: rangeSelectionMode!,
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(selectedDate, selectedDay)) {
+              widget.listCalendar?[1].value = selectedDay;
+              widget.listCalendar?[0].value = focusedDay;
+              widget.listCalendar?[2].value = null; // Important to clean those
+              widget.listCalendar?[3].value = null;
+              widget.listCalendar?[4].value = false;
+              widget.listCalendar?[6].value = RangeSelectionMode.toggledOff;
+            }
+          },
+          onRangeSelected: (start, end, focusedDay) {
+            widget.listCalendar?[1].value = null;
+            widget.listCalendar?[0].value = focusedDay;
+            widget.listCalendar?[2].value = start;
+            widget.listCalendar?[3].value = end;
+            widget.listCalendar?[4].value = false;
+            widget.listCalendar?[6].value = RangeSelectionMode.toggledOn;
+
+            widget.callData!(widget.query ?? '', 1, start!, end!);
+          },
+          onFormatChanged: (format) {
+            if (formatCalendar != format) {
+              widget.listCalendar?[5].value = format;
+            }
+          },
+          onPageChanged: (focusedDay) {
+            widget.listCalendar?[0].value = focusedDay;
+          },
+        );
+      },
+    );
+  }
+}
