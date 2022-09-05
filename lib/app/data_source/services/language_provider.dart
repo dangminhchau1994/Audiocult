416f2418ef17dcafe21328bad122de20b8d5206a
hieu nguyen <nt.hieu7393@gmail.com>
2022-09-05 17:05:19 +0700

    fix unexpected token expired related to language

--- lib/app/data_source/services/language_provider.dart
+++ lib/app/data_source/services/language_provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:audio_cult/app/data_source/local/pref_provider.dart';
 import 'package:audio_cult/app/data_source/models/responses/localized_text.dart';
 import 'package:audio_cult/app/data_source/services/app_service_provider.dart';
 import 'package:audio_cult/app/data_source/services/assets_local_provider.dart';
@@ -12,6 +13,7 @@ class LanguageProvider {
   late AssetsLocalServiceProvider _assetProvider;
   late AppServiceProvider _appServiceProvider;
   late LocalizedText? _localizedText;
+  late PrefProvider? _prefProvider;
 
   final _localizedTextStreamController = StreamController<LocalizedText>.broadcast();
 
@@ -22,14 +24,20 @@ class LanguageProvider {
   LanguageProvider({
     required AssetsLocalServiceProvider assetProvider,
     required AppServiceProvider appServiceProvider,
+    required PrefProvider prefProvider,
   }) {
     _appServiceProvider = appServiceProvider;
     _assetProvider = assetProvider;
     _localizedText = LocalizedText.fromJson({});
+    _prefProvider = prefProvider;
     initLocalizedText('');
   }
 
   Future<void> initLocalizedText(String languageId) async {
+    if (_prefProvider?.isAuthenticated != true) {
+      _localizedText = await _assetProvider.getLocalizedText();
+      return;
+    }
     if (languageId.isNotEmpty == true) {
       try {
         final localizedTextData = await _appServiceProvider.getLocalizedTextData(languageId);
